6239a040f11ccc48e03729a1a6e17f273c818b84
Leyenaar Lab <seneca.d.freyleue@dartmouth.edu>
2024-09-26 08:48:48 -0400

    Update PCORI Interaction modeling.R

--- PCORI Interaction modeling.R
+++ PCORI Interaction modeling.R
@@ -59,22 +59,19 @@ All_primary$D2 <- as.factor(All_primary$D2)
 
 ###Admit diagnosis Models###
 fit1ad <- glmer(first_clinical_assessment ~ D1 + age2 + gender +
-                    payer + race + pmca + unstable_bp +
-                    unstable_hr + unstable_resp + unstable_tp + unstable_o2 + language + brief_comp +
+                    payer + race + pmca + language + brief_comp +
                     site + arrive_block + arrive_block*site +
                     (1 | Practice.group), data=All_primary, family = Gamma(link = "log"))
 summary(fit1ad)
 vcov(fit1ad)
 fit2ad <- glmer(first_labs_imaging ~ D1 + age2 + gender +
-                    payer + race + pmca + unstable_bp +
-                    unstable_hr + unstable_resp + unstable_tp + unstable_o2 + language + brief_comp +
+                    payer + race + pmca + language + brief_comp +
                     site + arrive_block + arrive_block*site +
                     (1 | Practice.group), data=All_primary, family = Gamma(link = "log"))
 summary(fit2ad)
 vcov(fit2ad)
 fit3ad <- glmer(first_medication_admin ~ D1 + age2 + gender +
-                    payer + race + pmca + unstable_bp +
-                    unstable_hr + unstable_resp + unstable_tp + unstable_o2 + language + brief_comp +
+                    payer + race + pmca + language + brief_comp +
                     site + arrive_block + arrive_block*site +
                     (1 | Practice.group), data=All_primary, family = Gamma(link = "log"))
 summary(fit3ad)
@@ -83,22 +80,19 @@ vcov(fit3ad)
 
 ###PMCA models###
 fit1p <- glmer(first_clinical_assessment ~ D2 + age2 + gender +
-                    payer + admitreason + race + unstable_bp +
-                    unstable_hr + unstable_resp + unstable_tp + unstable_o2 + language + brief_comp +
+                    payer + admitreason + race + language + brief_comp +
                     site + arrive_block + arrive_block*site +
                     (1 | Practice.group), data=All_primary, family = Gamma(link = "log"))
 summary(fit1p)
 vcov(fit1p)
 fit2p <- glmer(first_labs_imaging ~ D2 + age2 + gender +
-                    payer + admitreason + race + unstable_bp +
-                    unstable_hr + unstable_resp + unstable_tp + unstable_o2 + language + brief_comp +
+                    payer + admitreason + race + language + brief_comp +
                     site + arrive_block + arrive_block*site +
                     (1 | Practice.group), data=All_primary, family = Gamma(link = "log"))
 summary(fit2p)
 vcov(fit2p)
 fit3p <- glmer(first_medication_admin ~ D2 + age2 + gender +
-                    payer + admitreason + race + unstable_bp +
-                    unstable_hr + unstable_resp + unstable_tp + unstable_o2 + language + brief_comp +
+                    payer + admitreason + race + language + brief_comp +
                     site + arrive_block + arrive_block*site +
                     (1 | Practice.group), data=All_primary, family = Gamma(link = "log"))
 summary(fit3p)
